c6b0b20a440c0a21c3d31742ba81f98f09c9d3fa
ForeverExiled <foreverexiled23@gmail.com>
2024-06-15 09:51:26 +0500

    Response class done

--- src/Response.php
+++ src/Response.php
@@ -72,8 +72,26 @@ class Response
         $this->header('Server', 'PHPServer/1.0.0');
     }
 
+    public function __toString()
+    {
+        return $this->buildHeaderString().$this->body;
+    }
+
     public function header($key, $value)
     {
         $this->headers[ucfirst($key)] = $value;
+    }
+
+    public function buildHeaderString()
+    {
+        $lines = [];
+
+        $lines[] = "HTTP/1.1 ".$this->status.' '.static::$statusCodes[$this->status];
+
+        foreach ($this->headers as $key => $value) {
+            $lines[] = $key.': '.$value;
+        }
+
+        return implode("\r\n", $lines)."\r\n\r\n";
     }
 }
